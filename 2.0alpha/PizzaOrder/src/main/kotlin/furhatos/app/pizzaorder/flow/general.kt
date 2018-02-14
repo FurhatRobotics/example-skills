a5519cdf09875b0b93d54bd196a6c99ff7748cba
luddilo <ludvig@furhatrobotics.com>
2018-02-14 10:49:09 +0100

    Added gradle files to FlightBooker and PizzaOrder

--- 2.0alpha/PizzaOrder/src/main/kotlin/furhatos/app/pizzaorder/flow/general.kt
+++ 2.0alpha/PizzaOrder/src/main/kotlin/furhatos/app/pizzaorder/flow/general.kt
@@ -1,9 +1,20 @@
 package furhatos.app.pizzaorder.flow
 
+import furhatos.flow.kotlin.furhat
+import furhatos.flow.kotlin.onUserLeave
+import furhatos.flow.kotlin.state
+
 import furhatos.flow.kotlin.*
 
 val Idle : State = state {
+    /*
+        On the first run only, if we have users in interaction
+        space, we attend a random user and start the interaction.
+        If not, we simply wait for a user to enter.
 
+        If we return to this state, we attend nobody and wait for
+        users to enter.
+     */
     init {
         if (users.count > 0) {
             furhat.attend(users.random)
@@ -22,7 +33,15 @@ val Idle : State = state {
 }
 
 val Interaction : State = state {
+    /*
+        Generic state to inherit for states where we are
+        attending a user.
 
+        If an attended user leaves, the system either
+        attends another user if existing or goes back to Idle.
+
+        If a user enters, we glance at the user.
+     */
     onUserLeave {
         if (users.count > 0) {
             if (it == users.current) {
@@ -39,5 +58,4 @@ val Interaction : State = state {
     onUserEnter {
         furhat.glance(it, 1)
     }
-
 }
