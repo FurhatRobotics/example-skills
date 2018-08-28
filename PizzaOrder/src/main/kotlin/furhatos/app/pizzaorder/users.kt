e02b019cd35935d1e502eb85e59f73e77941c2c2
luddilo <ludvig@furhatrobotics.com>
2018-08-28 17:16:13 +0200

    Removed PizzaOrder temporary due to incompatibility with the current SDK version

--- PizzaOrder/src/main/kotlin/furhatos/app/pizzaorder/users.kt
+++
@@ -1,8 +0,0 @@
-package furhatos.app.pizzaorder
-
-import furhatos.app.pizzaorder.nlu.OrderPizzaIntent
-import furhatos.flow.kotlin.NullSafeUserDataDelegate
-import furhatos.records.User
-
-// Associate an order to a user
-val User.order by NullSafeUserDataDelegate { OrderPizzaIntent() }
