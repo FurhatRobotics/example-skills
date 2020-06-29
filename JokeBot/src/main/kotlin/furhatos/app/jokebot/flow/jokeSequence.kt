f23c12073b7b612c9d389bf4bee9a369a5efb36f
quinten <quinten@furhatrobotics.com>
2020-06-29 17:50:26 +0200

    Slowly getting there

--- JokeBot/src/main/kotlin/furhatos/app/jokebot/flow/jokeSequence.kt
+++ JokeBot/src/main/kotlin/furhatos/app/jokebot/flow/jokeSequence.kt
@@ -2,45 +2,113 @@ package furhatos.app.jokebot.flow
 
 import furhatos.app.jokebot.jokes.JokeHandler
 import furhatos.flow.kotlin.*
+import furhatos.gestures.BasicParams
+import furhatos.gestures.Gestures
+import furhatos.gestures.defineGesture
 import furhatos.skills.emotions.UserGestures
+import furhatos.util.CommonUtils
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private val newIntros = listOf(
+        "I haven't tried this one before actually.",
+        "This is a new one."
+)
+
+private val neutralIntros = listOf(
+        "Let’s see what you think of this one.",
+        "Mixed reactions on this one so far."
+)
+
+private val positiveIntros = listOf(
+        "People usually like this one.",
+        "This one has been a success so far."
+)
+
+private val negativeIntros = listOf(
+        "This one is not liked by many, but maybe it's your kind of humor.",
+        "I haven’t had much luck with this one.",
+        "I’ll give this one another chance"
+)
+
+fun getJokeIntro(score: Double?) : String {
+    return when {
+        score == null -> newIntros.random()
+        score > 0.0 -> positiveIntros.random()
+        score < 0.0 -> negativeIntros.random()
+        else -> neutralIntros.random()
+    }
+}
 
 val JokeSequence: State = state {
 
     onEntry {
         val joke = JokeHandler.getJoke()
-        when {
-            joke.score == 0.0 -> {
-                furhat.say("I haven't tried this one before actually")
-            }
-            joke.score > 0.0 -> {
-                furhat.say("People usually like this one")
-            }
-            joke.score < 0.0 -> {
-                furhat.say("This one is not liked by many, but maybe it's your kind of humor.")
-            }
+        val utterance = utterance {
+            +getJokeIntro(joke.score)
+            +delay(200)
+            +joke.intro
+            +delay(1500) //Always let the intro sink in
+            +joke.punchline
         }
-        furhat.say(joke.intro)
+        furhat.say(utterance)
 
-        delay(1500) //Always let the intro sink in
-
-        furhat.say(joke.punchline)
         val jokeRanking = call(JokeScore) as Double
+        JokeHandler.changeJokeScore(jokeRanking)
+
+
     }
 }
 
-val JokeScore: State = state {
+val SmileBack: State = state {
+
+
+    val smileProbability = 0.25
+    val bigSmileProbability = 0.5
+    val smileTimeout = 5000L
+    var smilingIsAllowed = true
+    val resetAllowedToSmile = { GlobalScope.launch { delay(smileTimeout); smilingIsAllowed = true }} //Lambda function
+
+    onUserGesture(UserGestures.Smile, cond = { it.isCurrentUser && smilingIsAllowed}) {
+        val randomNumber = Math.random()
+        when {
+            randomNumber > bigSmileProbability -> {
+                smilingIsAllowed = false
+                furhat.gesture(indefiniteBigSmile)
+                resetAllowedToSmile()
+            }
+            randomNumber > smileProbability -> {
+                smilingIsAllowed = false
+                furhat.gesture(indefiniteSmile)
+                resetAllowedToSmile()
+            }
+            else -> {
+                //Nothing
+            }
+        }
+    }
+
+    onUserGestureEnd(UserGestures.Smile, cond = { it.isCurrentUser }) {
+        furhat.gesture(stopSmile)
+    }
+}
+
+val JokeScore: State = state(SmileBack) {
 
     var timeSmiledAtJoke: Long
     var timestampStartedLaughing = 0L
-    val jokeTimeout = 6000 // We wait for a smile for 6 seconds
+    val jokeTimeout = 6000 // We wait for a reaction for 6 seconds
     val jokeNegativeScore = 0.25
 
     onUserGesture(UserGestures.Smile, cond = { it.isCurrentUser }) {
         timestampStartedLaughing = System.currentTimeMillis()
+        propagate()
     }
 
     onUserGestureEnd(UserGestures.Smile, cond = { it.isCurrentUser }) {
         timeSmiledAtJoke = System.currentTimeMillis() - timestampStartedLaughing //Calculating the amount of millis spent laughing
+        //TODO FIX THIS
         terminate(calculateJokeScore(timeSmiledAtJoke, jokeTimeout))
     }
 
@@ -60,5 +128,45 @@ fun calculateJokeScore(timeSpentLaughing: Long, maxTimeLaughing: Int): Double {
         (maxTimeLaughing / timeSpentLaughing) * 0.5
     } else {
         0.0
+    }
+}
+
+
+/**
+ * The gestures are defined here.
+ */
+
+//Big Smile
+val indefiniteBigSmile = defineGesture {
+    frame(0.32, 0.64, persist = true) {
+        BasicParams.BROW_UP_LEFT to 1.0
+        BasicParams.BROW_UP_RIGHT to 1.0
+        BasicParams.SMILE_OPEN to 0.4
+        BasicParams.SMILE_CLOSED to 0.7
+    }
+}
+
+//Small smile
+val indefiniteSmile = defineGesture {
+    frame(0.32, 0.72, persist = true) {
+        BasicParams.SMILE_CLOSED to 0.5
+    }
+    frame(0.2, 0.72){
+        BasicParams.BROW_UP_LEFT to 1.0
+        BasicParams.BROW_UP_RIGHT to 1.0
+    }
+    frame(0.16, 0.72){
+        BasicParams.BLINK_LEFT to 0.1
+        BasicParams.BLINK_RIGHT to 0.1
+    }
+}
+
+//No more smiling
+val stopSmile = defineGesture {
+    frame(0.32, 0.64) {
+        BasicParams.BROW_UP_LEFT to 0.0
+        BasicParams.BROW_UP_RIGHT to 0.0
+        BasicParams.SMILE_OPEN to 0.0
+        BasicParams.SMILE_CLOSED to 0.0
     }
 }
--- JokeBot/src/main/kotlin/furhatos/app/jokebot/jokes/jokeHandler.kt
+++ JokeBot/src/main/kotlin/furhatos/app/jokebot/jokes/jokeHandler.kt
@@ -1,7 +1,10 @@
 package furhatos.app.jokebot.jokes
 
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import furhatos.util.CommonUtils
 
-val listOfJokes = listOf(
+val BASIC_LIST_OF_JOKES = listOf(
         Joke("What do robots eat as snacks", "micro-chips"),
         Joke("What do robots do at lunchtime", "have a mega-byte"),
         Joke("What happened when they shut down the robot motorway?", "Everyone had to take the R2 detour.", 1.0),
@@ -14,11 +17,14 @@ val listOfJokes = listOf(
         Joke("What is most important when telling jokes", "timing")
 )
 
-data class Joke(val intro: String, val punchline: String, var score: Double = 0.0)
+data class Joke(val intro: String, val punchline: String, var score: Double? = null)
 
 
 object JokeHandler {
 
+    private val jokeFile = CommonUtils.getAppDataDir("jokes.json") //File to read/save jokes to
+    private val gson = Gson().newBuilder().setPrettyPrinting().create() //JSON parser/serializer
+    private val listOfJokes = getJokes() //List of jokes
     private lateinit var currentJoke: Joke //Will be initialized when we request the first joke.
 
     /**
@@ -33,6 +39,45 @@ object JokeHandler {
      * Changes the current joke score with the change provided.
      */
     fun changeJokeScore(scoreChange: Double) {
-        currentJoke.score += scoreChange
+        if (currentJoke.score == null) {
+            currentJoke.score = 0.0
+        }
+        currentJoke.score = currentJoke.score!! + scoreChange
+        writeToFile()
     }
+
+    /**
+     * Tries to read the jokes from file. If that file does not exist, or the JSON cannot be parsed, we return a basic
+     * list of jokes.
+     */
+    private fun getJokes(): List<Joke> {
+        return if (jokeFile.exists()) {
+             try {
+                 gson.fromJson(jokeFile.readText(), Array<Joke>::class.java).toList()
+            } catch (_: JsonSyntaxException) {
+                BASIC_LIST_OF_JOKES
+            }
+        } else {
+            BASIC_LIST_OF_JOKES
+        }
+    }
+
+    /**
+     * Writes the current list of jokes to a file
+     */
+    private fun writeToFile() {
+        if (!jokeFile.exists()) {
+            jokeFile.createNewFile()
+        }
+        jokeFile.writeText(gson.toJson(listOfJokes))
+    }
+
+}
+
+fun main() {
+    println("Testing")
+    val thing = JokeHandler.getJoke()
+    println("Score now:" + thing.score)
+    JokeHandler.changeJokeScore(0.25)
+    println("New score:" + thing.score)
 }
