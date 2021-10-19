a5b919f0841150d3a985bed19b2e056c422f47a7
Kaspar Kööp <kaspar@furhatrobotics.com>
2021-10-19 14:02:42 +0200

    Moved tutorials to tutorials repository

--- FurGUI/assets/exampleGui/README.md
+++
@@ -1,3 +0,0 @@
-# Usage
-* `npm run watch` to only watch for/recompile on changes.
-* `npm run build` to generate a minified, production-ready build.
--- FurGUI/assets/exampleGui/src/index.js
+++
@@ -1,6 +0,0 @@
-// Render the top-level React component
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
-
-ReactDOM.render(<App />, document.getElementById('react-root'));
